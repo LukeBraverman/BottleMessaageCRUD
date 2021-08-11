e485d6b691e04ebd6c4e42a7efdf07c87e5376ae
lbrav <l.braverman.l@live.co.uk>
2021-08-11 14:21:37 +0100

    implementation 1st draft

--- src/main/java/com/mbottle/messagecrud/Controller/BottleMessageController.java
+++ src/main/java/com/mbottle/messagecrud/Controller/BottleMessageController.java
@@ -1,25 +1,42 @@
 package com.mbottle.messagecrud.Controller;
 
 import com.mbottle.messagecrud.Model.BottleMessage;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import com.mbottle.messagecrud.service.BottleMessageCRUDService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/BMAPI")
+@AllArgsConstructor
 public class BottleMessageController {
 
-    public void POSTMessageToDatabase() {
-    }
+    private BottleMessageCRUDService bottleMessageCRUDService;
 
+    @PostMapping("/postMessage")
+    public ResponseEntity<String> POSTMessageToDatabase(@RequestBody BottleMessage bottleMessage) {
+        bottleMessageCRUDService.addMessageToDatabase(bottleMessage);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Posted message to database");
+    }
+    @GetMapping("/getMessage")
     public BottleMessage GETMessageFORMDatabase() {
+        System.out.println("pinged2"
+        );
         BottleMessage bottleMessage = null;
         return bottleMessage;
     }
-
+    @DeleteMapping("/deleteMessage")
     public void DELETEMessageINDatabase() {
+        System.out.println("pinged3"
+        );
+    }
+    @PatchMapping("/updateMessage")
+    public void CHANGEMessageINDatabase() {
+        System.out.println("pinged4"
+        );
     }
 
-    public void CHANGEMessageINDatabase() {
-    }
 
 
 }
--- src/test/java/com/mbottle/messagecrud/controller/BottleMessageControllerTest.java
+++ src/test/java/com/mbottle/messagecrud/controller/BottleMessageControllerTest.java
@@ -1,6 +1,7 @@
 package com.mbottle.messagecrud.controller;
 
 import com.mbottle.messagecrud.Controller.BottleMessageController;
+import com.mbottle.messagecrud.Model.BottleMessage;
 import com.mbottle.messagecrud.service.BottleMessageCRUDService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,14 +9,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//readup on ssl security
 @ExtendWith(MockitoExtension.class)
 class BottleMessageControllerTest {
 
-    private MockMvc mvc;
+    private MockMvc mockMvc;
     @InjectMocks
     BottleMessageController bottleMessageController;
     @Mock
@@ -23,24 +31,140 @@ class BottleMessageControllerTest {
 
     @BeforeEach
     void setUp() {
-        mvc = MockMvcBuilders.standaloneSetup(bottleMessageController)
+        mockMvc = MockMvcBuilders.standaloneSetup(bottleMessageController)
                 .build();
     }
 
     @Test
-    void POSTMessageToDatabase() {
+    void POSTMessageToDatabase_Return201Created() throws Exception {
+        //given
+        String validJsonPayload = "{\"UID\":\"12345\"," +
+                " \"message\": \"testMessage\"" +
+                ",\"username\":" + "\"testUsername\"}";
+        doNothing().when(bottleMessageCRUDService).addMessageToDatabase(any(BottleMessage.class));
+        //when
+        mockMvc.perform(
+                post("/BMAPI/postMessage")
+                        .content(validJsonPayload)
+                        .contentType(MediaType.APPLICATION_JSON))
 
+                .andExpect(status().isCreated())
+                .andReturn();
+        //then
+        verify(bottleMessageCRUDService, times(1)).addMessageToDatabase(any(BottleMessage.class));
     }
 
     @Test
-    void GETMessageFORMDatabase() {
+    void POSTInvalidMessageToDatabase_Return400BadRequest() throws Exception {
+        //given
+        String InvalidJsonPayload = "{\"UID\":\"12345\"," +
+                "\"username\":" + "\"testUsername\"}";
+        doNothing().when(bottleMessageCRUDService).addMessageToDatabase(any(BottleMessage.class));
+        //when
+        mockMvc.perform(
+                post("/BMAPI/postMessage")
+                        .content(InvalidJsonPayload)
+                        .contentType(MediaType.APPLICATION_JSON))
+
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        //then
+        verify(bottleMessageCRUDService, times(0)).addMessageToDatabase(any(BottleMessage.class));
     }
 
     @Test
-    void DELETEMessageINDatabase() {
+    void GETMessageFORMDatabase_returnRandomMessageInDatabase() throws Exception {
+        //given
+        BottleMessage testBottleMessage = new BottleMessage();
+        testBottleMessage.setUID("12345");
+        testBottleMessage.setMessage("testMessage");
+        testBottleMessage.setUsername("testUsername");
+        when(bottleMessageCRUDService.getMessageInDatabase()).thenReturn(testBottleMessage);
+        String testBottleMessageJSON = "{\"UID\":\"12345\"," +
+                " \"message\": \"testMessage\"" +
+                ",\"username\":" + "\"testUsername\"}";
+        //when
+        mockMvc.perform(
+                get("/BMAPI/getMessage"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsSt))
+                .andReturn();
+        //then
+        verify(bottleMessageCRUDService,times(1)).getMessageInDatabase();
     }
 
     @Test
-    void CHANGEMessageINDatabase() {
+    void DELETEMessageINDatabase_Return200OKAndUID() throws Exception {
+        //given
+        String JSONPayloadWithUIDToDelete = "{\"UID\":\"12345\"}";
+
+        BottleMessage testBottleMessage = new BottleMessage();
+        testBottleMessage.setUID("12345");
+        testBottleMessage.setMessage("testMessage");
+        testBottleMessage.setUsername("testUsername");
+        doNothing().when(bottleMessageCRUDService).deleteMessageInDatabase(any(BottleMessage.class));
+
+        //when
+        mockMvc.perform(
+                delete("/BMAPI/deleteMessage")
+                        .content(JSONPayloadWithUIDToDelete)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        //then
+        verify(bottleMessageCRUDService,times(1)).deleteMessageInDatabase(any(BottleMessage.class));
+    }
+
+    @Test
+    void DELETEMessageWithInvalidJSON_Return400BadRequest() throws Exception {
+        //given
+        String INVALIDJSONPayloadWithUIDToDelete = "{\"INVALID\":\"12345\"}";
+        doNothing().when(bottleMessageCRUDService).deleteMessageInDatabase(any(BottleMessage.class));
+
+        //when
+        mockMvc.perform(
+                delete("/BMAPI/deleteMessage")
+                        .content(INVALIDJSONPayloadWithUIDToDelete)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        //then
+        verify(bottleMessageCRUDService,times(0)).deleteMessageInDatabase(any(BottleMessage.class));
+    }
+
+    @Test
+    void CHANGEMessageINDatabase_Return200OK() throws Exception {
+        //given
+        String JSONPayloadWithUIDToUpdate = "{\"UID\":\"12345\", " +
+                "\"message\": \"updatedMSG\"}";
+        doNothing().when(bottleMessageCRUDService).updateMessageInDatabase(any(BottleMessage.class));
+
+        //when
+        mockMvc.perform(
+                patch("/BMAPI/updateMessage")
+                        .content(JSONPayloadWithUIDToUpdate)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        //then
+        verify(bottleMessageCRUDService,times(1)).updateMessageInDatabase(any(BottleMessage.class));
+    }
+
+    @Test
+    void CHANGEInvalidMessageINDatabase_Return() throws Exception {
+        //given
+        String JSONPayloadWithUIDToUpdate = "{\"INVALID\":\"12345\", " +
+                "\"INVALID\": \"updatedMSG\"}";
+        doNothing().when(bottleMessageCRUDService).updateMessageInDatabase(any(BottleMessage.class));
+
+        //when
+        mockMvc.perform(
+                patch("/BMAPI/updateMessage")
+                        .content(JSONPayloadWithUIDToUpdate)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        //then
+        verify(bottleMessageCRUDService,times(0)).updateMessageInDatabase(any(BottleMessage.class));
     }
 }
