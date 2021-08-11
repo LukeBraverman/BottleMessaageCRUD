1c6677e7f1f1e1eedc16ed25cedd0c1a83ebd569
lbrav <l.braverman.l@live.co.uk>
2021-08-11 16:07:49 +0100

    Swagger updated

--- src/main/java/com/mbottle/messagecrud/config/SwaggerConfiguration.java
+++ src/main/java/com/mbottle/messagecrud/config/SwaggerConfiguration.java
@@ -2,6 +2,11 @@ package com.mbottle.messagecrud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,6 +17,21 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.mbottle.messagecrud.Controller"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("Employee Management REST API")
+                .contact(new Contact("Luke B", "www.MessageInABottle.net", "MessageBottlee@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
     }
 }
